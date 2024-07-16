c34fe00fef0478f05692f1e3259a96b8eeda05fb
AhmedIbrahim-tech <ebrahema89859@gmail.com>
2024-07-16 10:57:45 +0300

    [Update]

--- ECommerce.Client/src/app/shop/Components/product-details (Single-Product)/product-details.component.ts
+++ ECommerce.Client/src/app/shop/Components/product-details (Single-Product)/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ShopService } from '../../Services/shop.service';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/Models/Product';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-product-details',
@@ -11,19 +12,21 @@ import { IProduct } from 'src/app/shared/Models/Product';
 export class ProductDetailsComponent implements OnInit {
 
   product?: IProduct;
-  constructor(private _ShopService: ShopService, private _activeRoute: ActivatedRoute) { }
+  constructor(private _ShopService: ShopService, private _activeRoute: ActivatedRoute,private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
     this.loadproduct();
   }
 
   loadproduct() {
+    this.spinner.show();
     const id = this._activeRoute.snapshot.paramMap.get('id');
     if (id) {
       this._ShopService.getSingleProduct(+id).subscribe({
         next: (response: any) => 
         { 
           this.product = response.data;
+          this.spinner.hide();
         },
         error: (error) => { console.log(error) },
         complete: () => { console.log('load product complete') }
