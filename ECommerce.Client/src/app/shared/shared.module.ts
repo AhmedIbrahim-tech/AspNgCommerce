df53712f057b28218eadae4fb44332e00b635a0b
AhmedIbrahim-tech <ebrahema89859@gmail.com>
2024-07-16 09:37:55 +0300

    [Update]

--- ECommerce.Client/src/app/shared/shared.module.ts
+++ ECommerce.Client/src/app/shared/shared.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
-import { PagerComponent } from './Components/pager/pager.component';
 import { PagingHeaderComponent } from './Components/paging-header/paging-header.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 
 @NgModule({
   declarations: [
     PagingHeaderComponent,
-    PagerComponent,
   ],
   imports: [
     CommonModule,
@@ -21,17 +18,14 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
     CarouselModule.forRoot(),
     ReactiveFormsModule,
     BsDropdownModule.forRoot(),
-    NgxSpinnerModule,
-    PaginationModule.forRoot()
+    NgxSpinnerModule
   ],
   exports: [
     PagingHeaderComponent,
-    PagerComponent,
     CarouselModule,
     ReactiveFormsModule,
     BsDropdownModule,
-    NgxSpinnerModule,
-    PaginationModule
+    NgxSpinnerModule
   ]
 })
 export class SharedModule { }
